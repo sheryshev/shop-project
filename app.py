ab794240d401065cbca63d1f03231064bc298749
sheryshev <35301673+sheryshev@users.noreply.github.com>
2026-03-17 23:46:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def kafka_consumer_worker():
             enable_auto_commit=True,
             # Добавляем таймаут на запрос метаданных, чтобы не виснуть вечно
             request_timeout_ms=30000, 
-            value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+            #value_deserializer=lambda x: json.loads(x.decode('utf-8'))
         )
         
         # Проверка физического соединения
