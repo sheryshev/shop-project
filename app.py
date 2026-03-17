bc364dc3676270166a4788fb8591abf77bd169f6
sheryshev <35301673+sheryshev@users.noreply.github.com>
2026-03-18 00:53:08 +0300

    Update app.py
    port

--- app.py
+++ app.py
@@ -180,5 +180,5 @@ if __name__ == '__main__':
     consumer_thread.start()
 
     # Запускаем Flask сервер
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port, debug=False)
